b2f22f30e50a0aa812a2faba711492f1f18619e6
uduak gabriel <uduakgabriel507@gmail.com>
2025-09-05 07:03:07 -1200

    deploying my projject to Render

--- job_board/job_board/settings.py
+++ job_board/job_board/settings.py
@@ -25,12 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*-t7=tss%6%2&043t$oi@*1m+h-8dm_z+pff7v&z*%qr0zd)u='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
-ALLOWED_HOSTS = ["uduak001.pythonanywhere.com",
-    "127.0.0.1",
-    "localhost",]
+ALLOWED_HOSTS = []
 
 
 # Application definition
