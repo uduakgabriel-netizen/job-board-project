a4894eb3866e498b8c7afc4f00cf82ae82bd0ab0
uduak gabriel <uduakgabriel507@gmail.com>
2026-04-17 09:10:21 -1200

    allowed

--- job_board/job/wsgi.py
+++ job_board/job/wsgi.py
@@ -10,6 +10,6 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job_board.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'job.settings')
 
 application = get_wsgi_application()
